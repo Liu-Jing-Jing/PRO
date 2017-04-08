3b9b1be5b789ccf6e0b934906b111dbde78ab374
wangjun <wjun@gmail.com>
2017-04-08 14:12:13 +0800

    add JsonTool

--- JAVA/PRO/src/main/com/pro/InvokeInterFace/AbstractServlet.java
+++ JAVA/PRO/src/main/com/pro/InvokeInterFace/AbstractServlet.java
@@ -2,6 +2,7 @@ package main.com.pro.InvokeInterFace;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import main.com.pro.tools.JsonTool;
 
 @SuppressWarnings("serial")
 @WebServlet("")
@@ -23,6 +26,8 @@ public class AbstractServlet extends HttpServlet{
 	private ServletOutputStream outstream;
 	
 	private PrintWriter writer;
+	
+	private Map FormData;
 
 	@Override
 	protected  void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -37,6 +42,8 @@ public class AbstractServlet extends HttpServlet{
 		this.response = resp;
 		this.outstream = resp.getOutputStream();
 		this.writer = resp.getWriter();
+		String data = request.getParameter("data");
+		this.FormData = JsonTool.ToMap(data);
 		doGet(req, resp);
 	}
 
---
+++ JAVA/PRO/src/main/com/pro/tools/JsonTool.java
@@ -0,0 +1,26 @@
+package main.com.pro.tools;
+
+import java.util.Map;
+
+public class JsonTool {
+	
+	private JsonTool() {
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static Map ToMap(String data){
+		return null;
+	}
+
+}
